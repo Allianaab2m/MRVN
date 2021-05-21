7464673011b4634aeb8b50b04ffb5b73fe0fae45
Hiro <hiro527.dev@gmail.com>
2021-05-22 01:31:41 +0900

    バージョンの更新、コマンドの追加、最適化

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const log4js = require('log4js');
-const token = require('./DISCORD_TOKEN.json')
-const commands = require('./lib/commands'); // コマンド設定ファイル
+const token = require('./DISCORD_TOKEN.json');
 const package = require('./package.json');
-const config = require('./config.json'); // Discordクライアント用
+// コマンド設定ファイル
+const commands = require('./lib/commands');
+// Discordクライアント用
+const config = require('./config.json');
 
 const client = new Discord.Client();
 
@@ -48,34 +50,38 @@ client.login(token.token).catch();
 // イベントハンドラ
 client.on('message', async message => {
     Object.keys(commands.commands).forEach((k) => {
-        let tmp = config.prefix + k;
+        const tmp = config.prefix + k;
         if (tmp === message.content) {
             switch (commands.commands[k].type) {
                 case 'text':
+                    // テキスト送信のみのコマンド
                     message.channel.send(commands.commands[k].text);
                     break;
                 case 'embed':
+                    // embed送信コマンド
                     message.channel.send({
-                        embed: commands.commands[k].embed
+                        embed: commands.commands[k].embed,
                     });
-                    break;
-                default: 
-                    message.reply('そのようなコマンドはありません。');
                     break;
             }
         }
-    })
+    });
 });
 
 client.on('guildMemberAdd', (member) => {
-    systemLog.info(`メンバー参加: ${client.users.cache.get(member.id).username}, UUID: ${member.id}`);
+    systemLog.info(`メンバー参加: ${client.users.cache.get(member.id).tag}(UUID: ${member.id})`);
     client.channels.cache.get('752185115995537532').send(`
     <:rank_predator:844950262337896478> <@${member.id}>さん、**${client.guilds.cache.get('751692700113305612').name}**へようこそ! <:rank_master:844950261953069097>
     <#752093558789505134>からルールを確認してください！
     `);
 });
 
+client.on('guildMemberRemove', (member) => {
+    systemLog.info(`メンバー削除: ${client.users.cache.get(member.id).tag}(UUID: ${member.id})`);
+});
+
 client.once('ready', () => {
     systemLog.info(`MRVN v.${package.version} 起動完了`);
-    client.user.setActivity('m!help | ApexM Japan')
+    systemLog.info(`ログイン成功: ${client.user.tag}(UUID:${client.user.id})`);
+    client.user.setActivity(`m!help | v.${package.version}`);
 });
